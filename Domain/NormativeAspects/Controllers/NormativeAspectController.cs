453a9debd8529edac8aa86d5a59b9813c8032f8b
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-09-21 23:55:54 -0500

    chore: void example to create CRUD's

--- Domain/NormativeAspects/Controllers/NormativeAspectController.cs
+++ Domain/NormativeAspects/Controllers/NormativeAspectController.cs
@@ -4,10 +4,39 @@ namespace programming_work_backend.Domain.NormativeAspects.Controllers;
 
 [ApiController]
 [Route("api/v1/normative-aspects")]
-public class NormativeAspectsController : ControllerBase{
+public class NormativeAspectsController : ControllerBase
+{
 
     [HttpGet]
-    public async Task<IActionResult> GetNormativeAspects(){
+    public async Task<IActionResult> GetNormativeAspects()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetOneNormativeAspects(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateNormativeAspect(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    [Route("{id}")]
+    public async Task<IActionResult> EditNormativeApect(int id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> DeleteNormativeAspects(int id)
+    {
         return Ok();
     }
 }
