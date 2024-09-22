cf7066d33fc468a72cea6dd8fd109ce00d7f0e41
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-09-21 23:23:35 -0500

    feat: now controllers are available to being read

--- Program.cs
+++ Program.cs
@@ -8,14 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "AllowAll",
         policy =>
         {
             policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
+                .AllowAnyMethod()
+                .AllowAnyHeader();
         });
 });
 // Db connection
@@ -33,9 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseCors("AllowAll");
-
+app.MapControllers();
 app.Run();
 
