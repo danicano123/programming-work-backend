4bbd8e64195998655a5c9b85a4c040c01bac1019
CarolinaGXx <carogt38@gmail.com>
2024-09-25 13:46:47 -0500

    feat: Allieds Model creation

---
+++ Domain/Allieds/Models/Allied.cs
@@ -0,0 +1,12 @@
+namespace programming_work_backend.Domain.Allieds.Models;
+
+public class Allied
+{
+
+    public string Nit { get; set; }
+    public string Company_reason { get; set; }
+    public string Contact_name { get; set; }
+    public string Phone { get; set; }
+    public string City { get; set; }
+
+}
