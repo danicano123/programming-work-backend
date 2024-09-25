7a548d3d69fd839d8901ff2bd9f78ce3271b6cf6
CarolinaGXx <carogt38@gmail.com>
2024-09-25 13:47:13 -0500

    feat: PracticeStrategy Model creation

---
+++ Domain/PracticeStrategys/Models/PracticeStrategy.cs
@@ -0,0 +1,11 @@
+namespace programming_work_backend.Domain.PracticeStrategys.Models;
+
+public class PracticeStrategy
+{
+
+    public string Id { get; set; }
+    public string Type_practice { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+}
