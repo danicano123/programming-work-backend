bf25956562aa2e8d84212e2fdf0814875dfe500f
andrwacevedo <andresacevedo858@gmail.com>
2024-09-26 04:34:25 -0500

    feat: add university and focus

--- Data/DBContext.cs
+++ Data/DBContext.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using programming_work_backend.Domain.Focus.Models;
 using programming_work_backend.Domain.NormativeAspects.Models;
+using programming_work_backend.Domain.University.Models;
 
 namespace programming_work_backend.Data;
 
@@ -13,4 +15,6 @@ public class DBContext : DbContext
     }
     // Models constructor
     public DbSet<NormativeAspect> NormativeAspects { get; set; }
+    public DbSet<University> University { get; set; }
+    public DbSet<Focus> Focus { get; set; }
 }
