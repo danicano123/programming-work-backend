7a407e3d29f63827e55f26857e60cea835e5d74f
andrwacevedo <andresacevedo858@gmail.com>
2024-09-26 04:32:58 -0500

    feat: created model university

---
+++ Domain/University/Models/University.cs
@@ -0,0 +1,10 @@
+namespace programming_work_backend.Domain.University.Models;
+
+public class University
+{
+
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string City { get; set; }
+
+}
